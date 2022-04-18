71e8c616971dd952da22f7a07e572c8b11937f3f
Nome <dickerson.nomi@gmail.com>
2022-04-18 12:45:03 -0700

    Only send shared trip notification once

--- notifications.js
+++ notifications.js
@@ -8,12 +8,24 @@ function sendSharedTripNotifications() {
   const ss = SpreadsheetApp.getActiveSpreadsheet()
   const sharedTrips = getRangeValuesAsTable(ss.getSheetByName("Trips")
     .getDataRange())
-    .filter(tripRow => tripRow['Share'] === true)
+    .filter(tripRow =>( tripRow['Share'] === true) && (!tripRow['Shared']))
   const numShared = sharedTrips.length
   if (numShared > 0) {
     let agencyName = getDocProp('providerName')
     let subject = agencyName + ' has shared trips with you'
-    let body = 'You have ' + numShared.toString() + ' shared trips waiting for review. To view shared trips, open the Outside Trips sheet in RideSheet, and use the API menu to "Get Trip Requests"'
+    let body = 'You have ' + numShared.toString() + ' shared trips waiting for review. To view shared trips, open the Outside Trips sheet in RideSheet. If the available trips are not visible, use the API menu to "Get Trip Requests"'
     sendNotification(subject, body)
+    markTripsAsShared(sharedTrips)
   }
+}
+
+function markTripsAsShared(sharedTrips) {
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const tripSheet = ss.getSheetByName("Trips")
+  const colUpdate = {'Shared': true}
+  sharedTrips.forEach(sharedTrip => {
+    let rowPosition = sharedTrip._rowPosition
+    let currentRow = tripSheet.getRange("A" + rowPosition + ":" + rowPosition)
+    setValuesByHeaderNames([colUpdate], currentRow)
+  })
 }
