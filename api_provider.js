d38d590a27c8f27be0897734e7ccb2745dbde84d
Kevin Chambers <kc@fullpath.io>
2024-03-05 23:58:29 -0800

    Merge remote-tracking branch 'origin/105-improve-address-handling' into clay-county

--- api_provider.js
+++ api_provider.js
@@ -1,263 +1,169 @@
 // Provider (this RideSheet instance) sends request for tripRequests to ordering client.
 // Received format is an array JSON objects, each element of which complies with
 // Telegram 1A of TCRP 210 Transactional Data Spec.
-function sendRequestForTripRequests() {
-  try {
-    const lastColumnLetter = "R"
-    const headers = [
-      "Scheduled PU Time",
-      "Decline",
-      "Claim",
-      "Source",
-      "Trip Date",
-      "Earliest PU Time",
-      "Requested PU Time",
-      "Latest PU Time",
-      "Requested DO Time",
-      "Appt Time",
-      "PU Address",
-      "DO Address",
-      "Guests",
-      "Mobility Factors",
-      "Notes",
-      "Est Hours",
-      "Est Miles",
-      "Trip ID"
-    ]
-    let grid = [
-      ["Last Updated:", new Date(), new Date()].concat(Array(headers.length-3).fill(null)),
-      headers
-    ]
-    let currentRow = 3
-    endPoints = getDocProp("apiGetAccess")
-    endPoints.forEach(endPoint => {
-      log("Name",endPoint.name)
-      if (endPoint.hasTrips) {
-        let params = {resource: "tripRequests"}
-        let response = getResource(endPoint, params)
-        let responseObject
-        try {
-          responseObject = JSON.parse(response.getContentText())
-          log(responseObject)
-        } catch(e) {
-          logError(e)
-          responseObject = {status: "LOCAL_ERROR:" + e.name}
-        }
-        let formatGroups = {
-          mergeCells: {
-            ranges: [],
-            formats: function(rl) {
-              rl.getRanges().forEach(range => range.merge())
-            }
-          },
-          header: {
-            ranges: ["A1:" + lastColumnLetter + "2"],
-            formats: function(rl) {
-              rl.setBackground(headerBackgroundColor)
-              rl.setFontWeight("bold")
-              rl.setBorder(true, true, true, true, null, null, "black", SpreadsheetApp.BorderStyle.SOLID_MEDIUM)
-            }
-          },
-          date: {
-            ranges: ["C1"],
-            formats: function(rl) {
-              rl.setNumberFormat("mm/dd/yyyy")
-              rl.setHorizontalAlignment("left")
-            }
-          },
-          time: {
-            ranges: ["B1"],
-            formats: function(rl) {
-              rl.setNumberFormat("h:mm am/pm")
-            }
-          },
-          duration: {
-            ranges: [],
-            formats: function(rl) {
-              rl.setNumberFormat("h:mm")
-            }
-          },
-          distance: {
-            ranges: [],
-            formats: function(rl) {
-              rl.setNumberFormat("0.00")
-            }
-          },
-          integer: {
-            ranges: [],
-            formats: function(rl) {
-              rl.setNumberFormat("0")
-              rl.setHorizontalAlignment("right")
-            }
-          },
-          checkbox: {
-            ranges: [],
-            formats: function(rl) {
-              rl.insertCheckboxes()
-            }
-          }
-        }
-        if (responseObject.status !== "OK") {
-          grid.push(
-            [responseObject.status].concat(Array(headers.length-1).fill(null))
-          )
-          const thisRange = "D" + currentRow + ":" + lastColumnLetter + currentRow
-          formatGroups.mergeCells.ranges.push(thisRange)
-          currentRow += 1
-        } else if (responseObject.results && responseObject.results.length) {
-          responseObject.results.forEach(item => {
-            const row = item.tripRequest
-            const openAttributes = JSON.parse(row["@openAttribute"])
-            grid.push([
-              null,
-              false,
-              false,
-              endPoint.name,
-              new Date(row.pickupTime["@time"]),
-              row.pickupWindowStartTime ? new Date(row.pickupWindowStartTime["@time"]) : null,
-              new Date(row.pickupTime["@time"]),
-              row.pickupWindowEndTime ? new Date(row.pickupWindowEndTime["@time"]) : null,
-              new Date(row.dropoffTime["@time"]),
-              row.appointmentTime ? new Date(row.appointmentTime["@time"]) : null,
-              buildAddressFromSpec(row.pickupAddress),
-              buildAddressFromSpec(row.dropoffAddress),
-              openAttributes.guestCount,
-              openAttributes.mobilityFactors,
-              openAttributes.notes,
-              openAttributes.estimatedTripDurationInSeconds / 86400,
-              openAttributes.estimatedTripDistanceInMiles,
-              openAttributes.tripTicketId
-            ])
-          })
-          formatGroups.checkbox.ranges.push("B" + currentRow + ":C" + (currentRow + responseObject.results.length - 1))
-          formatGroups.date.ranges.push("E" + currentRow + ":E" + (currentRow + responseObject.results.length - 1))
-          formatGroups.time.ranges.push("F" + currentRow + ":J" + (currentRow + responseObject.results.length - 1))
-          formatGroups.integer.ranges.push("M" + currentRow + ":M" + (currentRow + responseObject.results.length - 1))
-          formatGroups.duration.ranges.push("P" + currentRow + ":P" + (currentRow + responseObject.results.length - 1))
-          formatGroups.distance.ranges.push("Q" + currentRow + ":Q" + (currentRow + responseObject.results.length - 1))
-          currentRow += responseObject.results.length
-
-          const ss = SpreadsheetApp.getActiveSpreadsheet()
-          const sheet = ss.getSheetByName("Outside Trips") || ss.insertSheet("Outside Trips")
-          sheet.getDataRange().clear().breakApart()
-          let range = sheet.getRange(1, 1, grid.length, grid[0].length)
-          range.clearFormat()
-          range.setValues(grid)
-          applyFormats(formatGroups, sheet)
-          sheet.setFrozenRows(2)
-          sheet.setFrozenColumns(3)
-          sheet.autoResizeColumns(1,grid[0].length)
-          sheet.autoResizeRows(1,2)
-        } else {
-          log("No trip requests")
-          grid.push(
-            [null, null, null, endPoint.name + " responded with no trip requests"].concat(Array(14).fill(null))
-          )
-          const thisRange = "D" + currentRow + ":" + lastColumnLetter + currentRow
-          formatGroups.mergeCells.ranges.push(thisRange)
-
-          const ss = SpreadsheetApp.getActiveSpreadsheet()
-          const sheet = ss.getSheetByName("Outside Trips") || ss.insertSheet("Outside Trips")
-          sheet.getDataRange().clear().breakApart()
-          let range = sheet.getRange(1, 1, grid.length, grid[0].length)
-          range.clearFormat()
-          range.setValues(grid)
-          applyFormats(formatGroups, sheet)
-          sheet.setFrozenRows(2)
-          sheet.setFrozenColumns(3)
-          sheet.autoResizeColumns(1,grid[0].length)
-          sheet.autoResizeRows(1,2)
-          currentRow += 1
-        }
-      }
-    })
-  } catch(e) { logError(e) }
-}
-
-// Provider (this RideSheet instance) sends tripRequestResponses
-// (whether service for each tripRequest is available or not) to ordering client.
-// Send an array of tripRequestResponse JSON objects which comply with
-// Telegram 1B of TCRP 210 Transactional Data Spec.
-// Receive in response an array of clientOrderConfirmation JSON objects which comply with
-// Telegram 2A of TCRP 210 Transactional Data Spec.
-function sendTripRequestResponses() {
+function receiveTripRequest(tripRequest, senderId) {
+  log('Received TripRequest', JSON.stringify(tripRequest))
   try {
     const ss = SpreadsheetApp.getActiveSpreadsheet()
-    //TODO: Update so that only the relevant endpoint (the source) receives the responses
-    endPoints = getDocProp("apiGetAccess")
-    endPoints.forEach(endPoint => {
-      if (endPoint.hasTrips) {
-        let params = {resource: "tripRequestResponses"}
+    const tripSheet = ss.getSheetByName("Outside Trips")
+    const apiAccounts = getDocProp("apiGiveAccess")
+    const senderAccount = apiAccounts[senderId]
 
-        const trips = getRangeValuesAsTable(ss.getSheetByName("Outside Trips").getDataRange(), {headerRowPosition: 2}).filter(tripRow => {
-          return (
-            tripRow["Trip Date"] >=  dateToday() &&
-            ( tripRow["Decline"]   === true || tripRow["Claim"] === true) &&
-            !(tripRow["Decline"]   === true && tripRow["Claim"] === true)
-          )
-        })
-        let payload = trips.map(tripIn => {
-          let tripOut = {}
-          tripOut.tripAvailable = (tripIn["Claim"] === true)
-          if (tripOut.tripAvailable && trips["Scheduled PU Time"]) {
-            tripOut["scheduledPickupTime"] = {"@time": combineDateAndTime(tripIn["Trip Date"], tripIn["Scheduled PU Time"])}
-          }
-
-          let openAttributes = {}
-          openAttributes.tripTicketId = tripIn["Trip ID"]
-          tripOut["@openAttribute"] = JSON.stringify(openAttributes)
-
-          return {tripRequestResponse: tripOut}
-        })
-        // Here we're sending the tripRequestResponses in the payload.
-        // responseObject, if validated, contains clientOrderConfirmations
-        let response = postResource(endPoint, params, JSON.stringify(payload))
-        let responseObject
-        try {
-          log('Response', response.getContentText())
-          responseObject = JSON.parse(response.getContentText())
-        } catch(e) {
-          logError(e)
-          responseObject = {status: "LOCAL_ERROR:" + e.name}
-        }
-        if (responseObject.status === "OK") {
-          // Process clientOrderConfirmations
-          let {results} = responseObject
-          removeDeclinedTrips()
-          processClientOrderResponse(results, endPoint.name)
-          sendProviderOrderConfirmations(responseObject, endPoint)
-        }
+    const supportedFields = [
+      "tripTicketId",
+      "pickupAddress",
+      "dropoffAddress",
+      "pickupTime",
+      "dropoffTime",
+      "customerInfo",
+      "openAttributes",
+      "appointmentTime",
+      "notesForDriver",
+      "numOtherReservedPassengers"
+    ]
+    const tripRequestKeys = Object.keys(tripRequest)
+    const extraDataFields = tripRequestKeys.filter(key => !supportedFields.includes(key));
+    const extraInfo = extraDataFields.reduce((obj, key) => {
+      obj[key] = tripRequest[key];
+      return obj;
+    }, {});
+    // TODO: Format hours and miles? Either here or in the sheet
+    const tripData = {
+      'Source' : senderAccount.name,
+      'PU Address' : buildAddressFromSpec(tripRequest.pickupAddress),
+      'DO Address' : buildAddressFromSpec(tripRequest.dropoffAddress),
+      'Trip ID' : tripRequest.tripTicketId,
+      'Notes' : tripRequest.notesForDriver,
+      'Customer Info' : JSON.stringify(tripRequest.customerInfo),
+      'Extra Fields' : JSON.stringify(extraInfo),
+      'Guests' : tripRequest.numOtherReservedPassengers,
+      'Decline' : false,
+      'Claim' : false
+    }
+    let dateFlag = false
+    // Time fields are not required, check for at least one to get the trip date from
+    if (tripRequest.pickupTime && tripRequest.pickupTime.time) {
+      tripData["Trip Date"] = formatDate(tripRequest.pickupTime.time, null, 'M/d/yyyy')
+      tripData["Requested PU Time"] = formatDate(tripRequest.pickupTime.time, null, 'h:mm a')
+      dateFlag = true
+    }
+    if (tripRequest.dropoffTime && tripRequest.dropoffTime.time) {
+      if (!dateFlag) {
+        tripData["Trip Date"] = formatDate(tripRequest.dropoffTime.time, null, 'M/d/yyyy')
+        dateFlag = true
       }
-    })  
+      tripData["Requested DO Time"] = formatDate(tripRequest.dropoffTime.time, null, 'h:mm a')
+    }
+    if (tripRequest.appointmentTime && tripRequest.appointmentTime.time) {
+      if (!dateFlag) {
+        tripData["Appt Time"] = formatDate(tripRequest.appointmentTime.time, null, 'M/d/yyyy')
+        dateFlag = true
+      }
+      tripData["Requested DO Time"] = formatDate(tripRequest.appointmentTime.time, null, 'h:mm a')
+    }
 
-    return result
-  } catch(e) { logError(e) }
+    if (tripRequest.openAttributes) {
+      if (tripRequest.openAttributes.estimatedTripDurationInSeconds) {
+        tripData["Est Hours"] = tripRequest.openAttributes.estimatedTripDurationInSeconds / (60 * 60 * 24)
+      }
+      if (tripRequest.openAttributes.estimatedTripDistanceInMiles) {
+        tripData["Est Miles"] = tripRequest.openAttributes.estimatedTripDistanceInMiles
+      }
+    }
+    createRow(tripSheet, tripData)
+    return {status: "OK"}
+  } catch(e) {
+    logError(e)
+    return {status: "400"}
+  } 
 }
 
-function testOrderConfirmations() {
-  let response = [ { clientOrderConfirmations: 
-     { tripAvailable: true,
-       tripTicketId: '08eb4058-eae2-4dff-bb2b-f481b8cdf876',
-       '@customerId': 31,
-       '@openAttributes': '{"estimatedTripDurationInSeconds":10497,"estimatedTripDistanceInMiles":154.316245108}',
-       customerMobilePhone: '',
-       customerName: 'The Grey, Gandolf',
-       pickupWindowStartTime: null,
-       pickupWindowEndTime: null,
-       negotiatedPickupTime: null,
-       pickupTime: { '@time': '2021-10-30T20:31:36.107Z' },
-       dropoffTime: { '@time': '2021-10-30T20:31:36.108Z' },
-       appointmentTime: { '@time': '2021-10-30T20:31:36.108Z' },
-       dropoffAddress: { '@addressName': 'The Castle, Washington 98361, USA' },
-       pickupAddress: { '@addressName': '1005 W Burnside St, Portland, OR 97209, USA' } } },
-  { customerInfo: 
-     { customerId: 31,
-       customerName: 'The Grey, Gandolf',
-       customerAddress: { '@addressName': '18 S G St, Lakeview, OR 97630, USA' } 
-      }  
-  }]
-  let endpoint = {name: "Agency A"}
-  processClientOrderResponse(response, endpoint.name)
+function sendTripRequestResponses() {
+  try { 
+    const ss = SpreadsheetApp.getActiveSpreadsheet()
+    const outsideTrips = ss.getSheetByName("Outside Trips")
+    const trips = getRangeValuesAsTable(outsideTrips.getDataRange()).filter(tripRow => {
+      return (
+        tripRow["Trip Date"] >=  dateToday() &&
+        ( tripRow["Decline"]  === true || tripRow["Claim"] === true) &&
+        !(tripRow["Decline"]  === true && tripRow["Claim"] === true) &&
+        !(tripRow["Pending"] === 'TRUE') 
+      )
+    })
+    // For each trip: Check the 'Source column' and find the appropriate agency
+    const endPoints = getDocProp("apiGetAccess")
+    trips.forEach((trip) => {
+      const source = trip["Source"]
+      const endpoint = endPoints.find(endpoint => endpoint.name === source)
+      const params = {endpointPath: "/v1/TripRequestResponse"}
+      const claimed = trip["Claim"] === true
+      const telegram = {
+        tripTicketId: trip["Trip ID"],
+        tripAvailable: claimed
+      }
+      if (trip["Scheduled PU Time"]) {
+        telegram.scheduledPickupTime = {
+          time: combineDateAndTime(trip["Trip Date"], trip["Scheduled PU Time"])
+        }
+      }
+      try {
+        const response = postResource(endpoint, params, JSON.stringify(telegram))
+        const responseObject = JSON.parse(response.getContentText())
+          log('#1B response', responseObject)
+          if (responseObject.status === "OK") {
+            // Mark the response as "pending" -- wait for telegram #2A to actually handle a claim
+            // If the trip was declined, go ahead and process it now.
+            if (!claimed) {
+              safelyDeleteRow(outsideTrips, trip)
+            } else {
+              markTripAsPending(outsideTrips, trip)
+            }
+          } else {
+            // TODO: Handle the case where the 400-error is on a trip decline
+            log('Claim Trip Rejected', JSON.stringify(responseObject))
+            markTripAsFailure(outsideTrips, trip)
+          }
+      } catch (e) {
+        logError(e)
+      }
+    })
+  } catch (e) {
+    logError(e)
+  }
+}
+
+function markTripAsPending(sheet, tripRow) {
+  const headers = getSheetHeaderNames(sheet)
+  const headerPosition = headers.indexOf("Pending") + 1
+  const rowPosition = tripRow._rowPosition
+  const currentRow = sheet.getRange("A" + rowPosition + ":" + rowPosition)
+  currentRow.setBackgroundRGB(20,204,204)
+  currentRow.getCell(1,1).setNote('Trip claim pending')
+  currentRow.getCell(1, headerPosition).setValue("TRUE")
+}
+
+function markTripAsFailure(sheet, tripRow) {
+  const rowPosition = tripRow._rowPosition
+  const currentRow = sheet.getRange("A" + rowPosition + ":" + rowPosition)
+  currentRow.setBackgroundRGB(255,102,102)
+  currentRow.getCell(1,1).setNote('Claim rejected. See log for details.')
+}
+
+// Receive telegram #2A from ordering client
+function receiveClientOrderConfirmation(confirmation) {
+  const {tripTicketId, tripConfirmed} = confirmation
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const outsideTrips = ss.getSheetByName("Outside Trips")
+  const trips = getRangeValuesAsTable(outsideTrips.getDataRange())
+  const trip = trips.find(row => row["Trip ID"] === tripTicketId)
+  if (!tripConfirmed) {
+    markTripAsFailure(outsideTrips, trip)
+    return {status: "OK"}
+  }
+  const customerInfo = JSON.parse(trip["Customer Info"])
+  // Add customer 
+  return {status: "OK"}
+
 }
 
 function removeDeclinedTrips() {
@@ -265,20 +171,6 @@ function removeDeclinedTrips() {
   const outsideTrips = ss.getSheetByName('Outside Trips')
   const trips = getRangeValuesAsTable(outsideTrips.getDataRange(), {headerRowPosition: 2}).filter(tripRow => tripRow["Decline"] === true)
   safelyDeleteRows(outsideTrips, trips)
-}
-
-function processClientOrderResponse(payload, source) {
-  if (typeof payload === 'object' && payload.length) {
-    payload.forEach(telegram => {
-      if (telegram.clientOrderConfirmations) {
-        processClientOrderConfirmations(telegram.clientOrderConfirmations, source)
-      } else if (telegram.customerInfo) {
-        processCustomerInfo(telegram.customerInfo, source)
-      } else {
-        log('Warning: response type not recognized', telegram )
-      }
-    })
-  }
 }
 
 function processClientOrderConfirmations(confirmation, source) {
--- build.js
+++ build.js
@@ -18,7 +18,8 @@ function buildMenus() {
   menu.addSubMenu(settingsMenu)
   if (getDocProp("apiShowMenuItems")) {
     const menuApi = ui.createMenu('Ride Sharing')
-    menuApi.addItem('Get trip requests', 'sendRequestForTripRequests')
+    menuApi.addItem('Get trip requests (Deprecated)', 'sendRequestForTripRequests')
+    menuApi.addItem('Send trip requests', 'sendTripRequests')
     menuApi.addItem('Send responses to trip requests', 'sendTripRequestResponses')
     menuApi.addItem('Refresh outside runs', 'sendRequestForRuns')
     menuApi.addToUi()
