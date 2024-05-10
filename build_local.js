80a07c795dae1b1438e2ea614c47b61dab944a80
Nome <dickerson.nomi@gmail.com>
2024-05-10 14:39:44 -0700

    Initialize mkdocs branch

--- build_local.js
+++
@@ -1,6 +0,0 @@
-function buildLocalMenus() {
-  const ui = SpreadsheetApp.getUi()
-  const menu = ui.createMenu('CallSheet')
-  menu.addItem('Send referrals', 'sendCustomerReferrals')
-  menu.addToUi()
-}
