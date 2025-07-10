2aa425cc2e7906123276972434c9111a24729838
Kevin Chambers <kc@fullpath.io>
2025-07-10 01:45:21 -0700

    Update Add-on menu name to make it easier to apply
    to other agencies

--- build_local.js
+++ build_local.js
@@ -1,6 +1,6 @@
 function buildLocalMenus() {
   const ui = SpreadsheetApp.getUi()
-  const menu = ui.createMenu('Clay County')
+  const menu = ui.createMenu('RS Add-ons')
   menu.addItem('Refresh Dispatch Sheet', 'refreshDispatchSheetLocal')
   menu.addToUi()
 }
