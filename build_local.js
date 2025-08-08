ef0f40e292940e6aa1a6e14f5de5c05ec3e1ed57
Kevin Chambers <kc@fullpath.io>
2025-08-08 00:13:03 -0700

    Make add-on menu name more general for greater code portability

--- build_local.js
+++ build_local.js
@@ -1,6 +1,6 @@
 function buildLocalMenus() {
   const ui = SpreadsheetApp.getUi()
-  const menu = ui.createMenu('Marshall County')
+  const menu = ui.createMenu('RS Add-ons')
   menu.addItem('Create all-day manifest', 'createLocalManifestByDay')
   menu.addItem('Refresh Dispatch Sheet', 'refreshDispatchSheetLocal')
   menu.addToUi()
