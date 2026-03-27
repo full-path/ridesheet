84a860569f1d4d50410064eaa6c80e479d171905
Kevin Chambers <kc@fullpath.io>
2026-03-27 00:56:34 -0700

    Add daily summary manifests

--- build_local.js
+++ build_local.js
@@ -1,6 +1,7 @@
 function buildLocalMenus() {
   const ui = SpreadsheetApp.getUi()
   const menu = ui.createMenu('RS Add-ons')
+  menu.addItem('Generate daily trip summary','createLocalManifestByDay')
   menu.addItem('Refresh Dispatch Sheet', 'refreshDispatchSheetLocal')
   menu.addToUi()
 }
